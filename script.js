a0aad0dd732937ca2e36f61bb66a0d08a9fc741f
Xtract <xtractgamer@gmail.com>
2021-09-01 19:48:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -84,8 +84,9 @@ function a2hex(str) {
 function hex2a(hexx) {
     var hex = hexx.toString();//force conversion
     var str = '';
-    for (var i = 0; i < hex.length; i += 2)
+    for (var i = 0; i < hex.length; i += 2) {
         str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
+    }
     return str;
 }
 
